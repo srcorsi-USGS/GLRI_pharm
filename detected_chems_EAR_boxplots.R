cefd5a86f4cbc43a86f6032dc7886784b041e4c4
DeCicco <LDECICCO@USGS.GOV>
2018-11-06 09:38:55 -0600

    Bigger font

--- detected_chems_EAR_boxplots.R
+++ detected_chems_EAR_boxplots.R
@@ -1,4 +1,5 @@
 library(toxEval)
+library(dplyr)
 #NOTE: Add path to path_to_file!!!
 path_to_file <- 'processed_data/pharm_data.xlsx' 
 tox_list <- create_toxEval(path_to_file)
@@ -18,11 +19,11 @@ chemical_summary <- filter(chemical_summary,EAR>0)
 ######################################
 bio_plot <- plot_tox_boxplots(chemical_summary, 
                               category = 'Chemical',
-                              mean_logic = FALSE,
+                              mean_logic = FALSE,font_size = 14,
                               hit_threshold = NA,
                               title = 'Summing EARs for all chemicals of a sample, taking the max of each site',
                               plot_ND = TRUE)
 bio_plot
-png("plots/detected_chem_boxplots.png", width = 1000, height = 800, res = 142)
+png("plots/detected_chem_boxplots.png", width = 1000, height = 600, res = 100)
 grid::grid.draw(bio_plot)
 dev.off()
