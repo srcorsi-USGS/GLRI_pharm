55013a2cfaaea153a8b48c64fc5fc4e6ad8f05ef
srcorsi-USGS <srcorsi@usgs.gov>
2019-02-14 12:19:20 -0600

    more chem prioritization

--- Prioritize_chems_for_bioeffect_analysis.R
+++ Prioritize_chems_for_bioeffect_analysis.R
@@ -9,13 +9,17 @@ path_to_file <- 'processed_data/pharm_data.xlsx'
 tox_list <- create_toxEval(path_to_file)
 
 chem_data <- as.data.frame(tox_list$chem_data)
-
+chem_info <- as.data.frame(tox_list$chem_info)
 
 chem_detected <- chem_data %>% 
   group_by(CAS) %>% 
-  summarize(conc_max = max(Value)),
+  summarize(conc_max = max(Value),
             conc_med = median(Value),
             conc_min = median(Value),
             num_samples = sum(!is.na(Value)),
-            num_detections = sum(Value>0)) %>%
-  arrange(conc_max)
+            num_detections = sum(Value>0))
+
+chem_detected <- left_join(chem_info[,c("CAS","parameter_nm")],chem_detected) %>%
+  arrange(desc(conc_max))
+
+write.csv(chem_detected,file="./Analysis not pushed/Pharms_detected.csv",row.names = FALSE)
